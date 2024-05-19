b8e2c43574cdae4e0f53e14ccffc37b410db0de7
Andrey Arthur <andrey_arthur1@outlook.com>
2024-05-19 00:54:53 -0300

    use game in main file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
-#include "include/module.h"
+#include "include/game.h"
 #include <stdio.h>
 
 int main() {
-    printf("1 + 2 = %d\n", module_add(1, 2));
+    Game game = game_init();
+
+    for (int row = 0; row < GAME_SIZE; row++) {
+        for (int column = 0; column < GAME_SIZE; column++) {
+            if (column == GAME_SIZE - 1) {
+                printf(" %c \n", game.board[row][column]);
+            } else {
+                printf(" %c |", game.board[row][column]);
+            }
+        }
+
+        if (row != GAME_SIZE - 1) {
+            printf("-----------\n");
+        }
+    }
     return 0;
 }
