792a239308ccff5117882dc14cf1181fe566308b
Andrey Arthur <andrey_arthur1@outlook.com>
2024-05-25 11:40:11 -0300

    implement game and test it

--- src/game.c
+++ src/game.c
@@ -10,3 +10,97 @@ Game game_init() {
     game.turn = 'X';
     return game;
 }
+
+bool game_position_is_empty(Game* game, int row, int column) {
+    if (game->board[row][column] == ' ') {
+        return true;
+    }
+    return false;
+}
+
+void game_insert(Game* game, int row, int column) {
+    game->board[row][column] = game->turn;
+    if (game->turn == 'X') {
+        game->turn = 'O';
+    } else {
+        game->turn = 'X';
+    }
+}
+
+static char game_verify_row(Game* game) {
+    for (int index = 0; index < GAME_SIZE; index++) {
+        if (game->board[index][0] != ' ' &&
+            game->board[index][0] == game->board[index][1] &&
+            game->board[index][1] == game->board[index][2]) {
+            return game->board[index][0];
+        }
+    }
+    return ' ';
+}
+
+static char game_verify_column(Game* game) {
+    int column = 0;
+    while (column < GAME_SIZE) {
+        if ((game->board[0][column] == 'O') &&
+            (game->board[1][column] == 'O') &&
+            (game->board[2][column] == 'O')) {
+            return 'O';
+        } else if (
+            (game->board[0][column] == 'X') &&
+            (game->board[1][column] == 'X') &&
+            (game->board[2][column] == 'X')) {
+            return 'X';
+        }
+        column++;
+    }
+    return ' ';
+}
+
+static char game_verify_diagonal(Game* game) {
+    if (game->board[0][0] != ' ' && game->board[0][0] == game->board[1][1] &&
+        game->board[1][1] == game->board[2][2]) {
+        return game->board[0][0];
+    }
+
+    if (game->board[0][2] != ' ' && game->board[0][2] == game->board[1][1] &&
+        game->board[1][1] == game->board[2][0]) {
+        return game->board[0][2];
+    }
+
+    return ' ';
+}
+
+static bool game_is_full(Game* game) {
+    bool is_full = true;
+    for (int row = 0; row < GAME_SIZE; row++) {
+        for (int column = 0; column < GAME_SIZE; column++) {
+            if (game->board[row][column] == ' ') {
+                is_full = false;
+            }
+        }
+    }
+    return is_full;
+}
+
+char game_verify_win(Game* game) {
+    char winner = game_verify_row(game);
+    if (winner != ' ') {
+        return winner;
+    }
+
+    winner = game_verify_column(game);
+    if (winner != ' ') {
+        return winner;
+    }
+
+    winner = game_verify_diagonal(game);
+    if (winner != ' ') {
+        return winner;
+    }
+
+    if (game_is_full(game)) {
+        return '=';
+    }
+
+    return ' ';
+}
--- tests/game_test.c
+++ tests/game_test.c
@@ -17,8 +17,64 @@ bool test_game_init() {
     return is_cleaned && game.turn == 'X';
 }
 
+bool test_game_position_is_empty() {
+    Game game = game_init();
+    game.board[0][1] = 'X';
+
+    bool empty = game_position_is_empty(&game, 0, 0);
+    bool not_empty = game_position_is_empty(&game, 0, 1);
+
+    return empty == true && not_empty == false;
+}
+
+bool test_game_insert() {
+    Game game = game_init();
+    game_insert(&game, 0, 0);
+    game_insert(&game, 0, 1);
+
+    return game.board[0][0] == 'X' && game.board[0][1] == 'O';
+}
+
+bool test_game_verify_win() {
+    Game no = game_init();
+
+    Game x = game_init();
+    x.board[0][0] = 'X';
+    x.board[0][1] = 'X';
+    x.board[0][2] = 'X';
+
+    Game o = game_init();
+    o.board[0][0] = 'O';
+    o.board[1][0] = 'O';
+    o.board[2][0] = 'O';
+
+    Game diagonal = game_init();
+    diagonal.board[0][2] = 'O';
+    diagonal.board[1][1] = 'O';
+    diagonal.board[2][0] = 'O';
+
+    Game draw = game_init();
+    draw.board[0][0] = 'O';
+    draw.board[0][1] = 'X';
+    draw.board[0][2] = 'O';
+    draw.board[1][0] = 'X';
+    draw.board[1][1] = 'O';
+    draw.board[1][2] = 'X';
+    draw.board[2][0] = 'X';
+    draw.board[2][1] = 'O';
+    draw.board[2][2] = 'X';
+
+    return game_verify_win(&no) == ' ' && game_verify_win(&x) == 'X' &&
+           game_verify_win(&o) == 'O' && game_verify_win(&diagonal) == 'O' &&
+           game_verify_win(&draw) == '=';
+}
+
 int main() {
     Runner runner = runner_init("game");
     runner.test("should init with correct values", test_game_init);
+    runner.test(
+        "should ensure that position is empty", test_game_position_is_empty);
+    runner.test("should insert in the position", test_game_insert);
+    runner.test("should verify the win", test_game_verify_win);
     return 0;
 }
