1e960a379fc6697218884f2272d12c156d3019e9
Andrey Arthur <andrey_arthur1@outlook.com>
2024-05-25 11:39:56 -0300

    define game functions

--- src/include/game.h
+++ src/include/game.h
@@ -1,6 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdbool.h>
 #define GAME_SIZE 3
 
 typedef struct Game {
@@ -9,5 +10,11 @@ typedef struct Game {
 } Game;
 
 Game game_init();
+
+bool game_position_is_empty(Game* game, int row, int column);
+
+void game_insert(Game* game, int row, int column);
+
+char game_verify_win(Game* game);
 
 #endif
